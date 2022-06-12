1479ed89c95269360c1ecf4659c18d5c9bd5cbbf
fleshofcat <fleshofcat@gmail.com>
2022-06-12 15:50:29 +0300

    Test custom logging of requests

---
+++ tests/test_custom_requests_logging.py
@@ -0,0 +1,18 @@
+import pyqt_rest_client as client
+from pyqt_rest_client.request import endpoint
+
+
+def some_endpoint():
+    return endpoint(str, ["some", "endpoint"])
+
+
+async def test_request_started_stopped_signals(login_mock, qtbot, qt_requests_mock):
+    qt_requests_mock.get("http://server:1234/api/v1.8/some/endpoint/")
+    request_notifier = client.request_notifier
+    request_description = "some description"
+
+    with qtbot.wait_signal(request_notifier.request_finished) as request_finished:
+        with qtbot.wait_signal(request_notifier.request_started) as request_started:
+            await some_endpoint().get(descr=request_description)
+
+    assert request_finished.args == request_started.args == [request_description]
